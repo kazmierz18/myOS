778859e660dc3d2f60154410b9d8c958ef70d195
Luca <kazmierz18@gmail.com>
2016-08-04 18:29:58 +0200

    Change in run script for new structure of files and folders

--- testOs.sh
+++ testOs.sh
@@ -1,10 +1,22 @@
-echo Enetring .bootloader...
+echo 'Enetring .bootloader...'
 cd ./bootloader
-echo Compiling source if needed...
-make
-echo Enering root folder...
+echo 'Compiling bootloader...'
+make	#compile all bootloader files
+echo 'Enering os folder...'
+cd ../kernel #compile 
+echo 'Compiling system kernel...'
+make	#compile all kernel files
+echo 'Enering root folder...'
 cd ..
-echo Writing OS to virtual disk...
-dd if=bootloader/boot.bin of=myos.img
-echo Starting OS...
-qemu-system-i386 myos.img
+if [ ! -f "myos.img" ]
+then
+	echo 'Creating virtal disk for qemu...'
+	qemu-img create -f qcow2 myos.img 1M #create imgae file for qemu if not exist
+fi
+echo 'Writing OS to virtual disk...'
+dd if=bootloader/bin/boot.bin of=myos.img		#put in first boot sector
+												#here should be file tabel in 2nd sector
+dd if=bootloader/binboot2.bin of=myos.img seek=2 #skip to the 3 sector and put here 2nd stage bootloader
+hexdump myos.img	# display hex map of disk
+echo 'Starting machine..'
+qemu-system-i386 myos.img #run Virtual PC
